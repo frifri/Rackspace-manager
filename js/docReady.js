e6ca2b8e188159ef731edb24aa56f8b28a5e90db
Francis Genet <58907@supinfo.com>
2012-01-29 19:45:42 +0100

    The username and the api key are now localy stored.

--- js/docReady.js
+++ js/docReady.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	
+	// Loading the login values
+	$('.content #settings_container input#username').val(localStorage['username']);
+	$('.content #settings_container input#apikey').val(localStorage['apikey']);
+	// ------------------------
+
+	// Saving settings
+	$('.content #settings_container img#creds_validate').click(function() {
+		var username = $('.content #settings_container input#username').val();
+		var apikey = $('.content #settings_container input#apikey').val();
+
+		if(username != '' && apikey != '') {
+			localStorage['username'] = username;
+			localStorage['apikey'] = apikey;
+		} else {
+			console.log("an empty input... Boloss!")
+		}
+	});
 });
